a39e2c5536786e6ac040e19f988b7cde8d0759a3
Patrick Cunningham <patrickcunningham1400@gmail.com>
2022-10-12 12:07:53 -0500

    more node practice

--- node-crash-course/files.js
+++ node-crash-course/files.js
@@ -1,9 +1,16 @@
 const fs = require('fs');
 
-fs.readFile('./docs/blog1.txt', (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
-});
+// fs.readFile('./docs/blog1.txt', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
 
+fs.writeFile('/docs/blog1.txt', "hello, world", () => {
+  console.log('file was written');
+})
+
+fs.writeFile('/docs/blog2.txt', "hello, world", () => {
+  console.log('file was written');
+})

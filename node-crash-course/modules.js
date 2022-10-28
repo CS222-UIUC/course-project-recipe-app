5cec194a4c568695e075ad177a1078626c342639
Patrick Cunningham <patrickcunningham1400@gmail.com>
2022-10-28 18:27:19 -0500

    more cleaning up

--- node-crash-course/modules.js
+++
@@ -1,7 +0,0 @@
-const { people, ages } = require('./people');
-
-console.log(people, ages);
-
-const os = require('os');
-
-console.log(os.platform(), os.homedir());
--- node-crash-course/people.js
+++
@@ -1,7 +0,0 @@
-const people = ['Conor', 'Patrick', 'Joe', 'Gabe'];
-const ages = [23, 56, 76, 34];
-console.log(people);
-
-module.exports = {
-  people, ages
-};
